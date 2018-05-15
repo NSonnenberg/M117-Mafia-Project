afc1f40eb44637c463f29d4f2aabca9fcf14592c
NSonnneberg <sonnenbergneil@gmail.com>
2018-05-15 15:10:12 -0700

    added message object

---
+++ app/src/main/java/com/example/keiji/app/objects/Message.java
@@ -0,0 +1,13 @@
+package com.example.keiji.app.objects;
+
+public class Message {
+    private String message;
+    private String type;
+
+    public Message(String type, String message) {
+        this.type = type;
+        this.message = message;
+    }
+
+
+}
