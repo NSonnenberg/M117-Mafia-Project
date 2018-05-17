1c0d9a0f624e3ee577fa193b26a1f6e0be7cbb88
Nikita Lukyanenko <nikita1923666@gmail.com>
2018-05-16 22:06:53 -0700

    created rules activity

---
+++ app/src/main/java/com/example/keiji/app/activities/GeneralRules.java
@@ -0,0 +1,20 @@
+package com.example.keiji.app.activities;
+
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class GeneralRules extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View result = inflater.inflate(R.layout.fragment_general_rules, container, false);
+        TextView rulesText = result.findViewById(R.id.general_view);
+        rulesText.setMovementMethod(new ScrollingMovementMethod());
+        return result;
+    }
+}
---
+++ app/src/main/java/com/example/keiji/app/activities/VilagerRules.java
@@ -0,0 +1,15 @@
+package com.example.keiji.app.activities;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class VilagerRules extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_vilager_rules, container, false);
+    }
+}
